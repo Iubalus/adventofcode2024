a517fa57b1cc895903abce4c0095ff37498294b2
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2024-12-07 15:11:03 -0700

    Day 7 Part 1 Solution adjust comment

--- src/main/java/com/jubalrife/aoc24/Day7BridgeRepair.java
+++ src/main/java/com/jubalrife/aoc24/Day7BridgeRepair.java
@@ -1,8 +1,6 @@
 package com.jubalrife.aoc24;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,35 +40,34 @@ public class Day7BridgeRepair {
         char[] operators = new char[args.size() - 1];
         Arrays.fill(operators, '+');
         for (int i = 0; i < Math.pow(2, operators.length); i++) {
-
-            /***
+            /*
              * They didn't honor order of operations so to solve the puzzle I have to do math incorrectly :/
              * Sample says this is valid "292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20"
              * When in fact that would equal 11 + (6 *16) + 20 = 127 which is not 292.
-             List<BigInteger> results = new ArrayList<>();
-             BigInteger current = null;
-             for (int j = 0; j < operators.length; j++) {
-             if (operators[j] == '*') {
-             if (current == null) {
-             current = args.get(j);
-             }
-             current = current.multiply(args.get(j + 1));
-             } else {
-             if (current != null) {
-             results.add(current);
-             current = null;
-             } else {
-             results.add(args.get(j));
-             }
-             }
-             }
-             if (current != null) {
-             results.add(current);
-             } else {
-             results.add(args.get(args.size() - 1));
-             }
-             BigInteger total = results.stream().reduce(BigInteger.ZERO, BigInteger::add);
-             (**/
+            List<BigInteger> results = new ArrayList<>();
+            BigInteger current = null;
+            for (int j = 0; j < operators.length; j++) {
+                if (operators[j] == '*') {
+                    if (current == null) {
+                        current = args.get(j);
+                    }
+                    current = current.multiply(args.get(j + 1));
+                } else {
+                    if (current != null) {
+                        results.add(current);
+                        current = null;
+                    } else {
+                        results.add(args.get(j));
+                    }
+                }
+            }
+            if (current != null) {
+                results.add(current);
+            } else {
+                results.add(args.get(args.size() - 1));
+            }
+            BigInteger total = results.stream().reduce(BigInteger.ZERO, BigInteger::add);
+            */
             BigInteger total = args.get(0);
             for (int j = 0; j < operators.length; j++) {
                 if (operators[j] == '+') {
